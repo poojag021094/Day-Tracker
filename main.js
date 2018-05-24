5e99a641cc356e703a64e14088096c4fcff59407
poojag021094 <poojag021094@gmail.com>
2018-05-24 07:31:37 +0530

    Day Tracker

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  var d = new Date();
+  var day = d.getDay();
+  console.log(day);
+
+  var msg = ["Sunday!!! Funcday!!",
+            "Back on the grind!",
+            "Oh. Its Tuesday!",
+            "Boom! Wednesday!",
+            "Thirsty, Thursday! Yo!",
+            "t's Friday! We should be kickin' it!",
+            "Paaarrrttyyyyy!"]
+});
